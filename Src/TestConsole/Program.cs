eff37332c5c0219563465c8b4a10190d607d8caf
Jongho Yang <jongho.yang@gmail.com>
2016-03-05 15:24:52 +0900

    Db test code

--- Src/TestConsole/Program.cs
+++ Src/TestConsole/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MTree.Configuration;
 using System.Threading;
+using MTree.DbProvider;
+using MTree.DataStructure;
 
 namespace TestConsole
 {
@@ -14,13 +16,39 @@ namespace TestConsole
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Config.Daishin.AccountPw);
+            //TestDbSaving();
+        }
 
-            //while (true)
-            //{
-            //    logger.Info("hhhhhhh");
-            //    Thread.Sleep(500);
-            //}
+        private static void TestDbSaving()
+        {
+            try
+            {
+                MongoDbProvider.Instance.Connect();
+
+                // stock master
+                var stockMaster = new StockMaster();
+                var stockMasterCollection = MongoDbProvider.Instance.GetDatabase(DbType.StockMaster).GetCollection<StockMaster>(Config.Default.MongoDbDateCollectionName);
+                stockMasterCollection.InsertOne(stockMaster);
+
+                // stock conclusion
+                var stockConclusion = new StockConclusion();
+                var stockConclusionCollection = MongoDbProvider.Instance.GetDatabase(DbType.StockConclusion).GetCollection<StockConclusion>(Config.Default.MongoDbDateCollectionName);
+                stockConclusionCollection.InsertOne(stockConclusion);
+
+                // index conclusion
+                var indexConclusion = new IndexConclusion();
+                var indexConclusionCollection = MongoDbProvider.Instance.GetDatabase(DbType.IndexConclusion).GetCollection<IndexConclusion>(Config.Default.MongoDbDateCollectionName);
+                indexConclusionCollection.InsertOne(indexConclusion);
+
+                // bidding price
+                var biddingPrice = new BiddingPrice();
+                var biddingPriceCollection = MongoDbProvider.Instance.GetDatabase(DbType.BiddingPrice).GetCollection<BiddingPrice>(Config.Default.MongoDbDateCollectionName);
+                biddingPriceCollection.InsertOne(biddingPrice);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
         }
     }
 }
