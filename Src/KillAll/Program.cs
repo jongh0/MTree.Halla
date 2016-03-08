0a225c42702dda851068be221902bcbdccd97506
Jongho Yang <jongho.yang@gmail.com>
2016-03-08 22:00:56 +0900

    KillAll Project 추가

---
+++ Src/KillAll/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KillAll
+{
+    class Program
+    {
+        static string[] proccessNameList = {
+            "MTree.HistorySaver",
+            "MTree.DashBoard",
+            "MTree.DaishinPublisher",
+            "MTree.KrxPublisher",
+            "MTree.EbestPublisher",
+            "MTree.KiwoomPublisher",
+            "MTree.RealTimeProvider",
+            "MTree.DaishinSessionManager",
+            "MTree.DaishinPopupStopper",
+            "TestConsole",
+            "TestConsumer",
+            "TestPublisher"
+        };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var processName in proccessNameList)
+                {
+                    KillProcess(processName);
+                }
+
+                Thread.Sleep(200);
+
+                foreach (var processName in proccessNameList)
+                {
+                    KillProcess(processName);
+                }
+
+                Thread.Sleep(500);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static void KillProcess(string processName)
+        {
+            var processList = Process.GetProcessesByName(processName);
+            if (processList == null)
+                return;
+
+            foreach (var p in processList)
+            {
+                try
+                {
+                    p.Kill();
+                    Console.WriteLine($"{p.ProcessName}, {p.Id} process killed");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"{p.ProcessName}, {p.Id} has a problem");
+                }
+            }
+        }
+    }
+}
