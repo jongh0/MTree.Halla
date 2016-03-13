9963a18783526dec54351e0ac3e615f82bcce9a4
Jongho Yang <jongho.yang@gmail.com>
2016-03-13 22:54:10 +0900

    KiwoomPublisher Icon 적용

--- Src/MTree.KiwoomPublisher/MainWindow.Designer.cs
+++ Src/MTree.KiwoomPublisher/MainWindow.Designer.cs
@@ -43,15 +43,16 @@
             this.axKHOpenAPI.TabIndex = 0;
             this.axKHOpenAPI.Visible = false;
             // 
-            // Form1
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.axKHOpenAPI);
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
             this.Text = "KiwoomPublisher";
-			((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI)).EndInit();
             this.ResumeLayout(false);
 
         }
