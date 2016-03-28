ea3a27204366b5c74ae5309733a90b388b8f5edf
Jongho Yang <jongho.yang@gmail.com>
2016-03-28 21:54:59 +0900

    Dashboard item update 조건 추가

--- Src/MTree.Dashboard/DashboardItem.cs
+++ Src/MTree.Dashboard/DashboardItem.cs
@@ -10,46 +10,82 @@ namespace MTree.Dashboard
 {
     public class DashboardItem : INotifyPropertyChanged
     {
-        private string _code;
+        private string _code = string.Empty;
         public string Code
         {
             get { return _code; }
-            set { _code = value;  NotifyPropertyChanged(nameof(Code)); }
+            set
+            {
+                _code = value;
+                NotifyPropertyChanged(nameof(Code));
+            }
         }
 
-        private string _name;
+        private string _name = string.Empty;
         public string Name
         {
             get { return _name; }
-            set { _name = value; NotifyPropertyChanged(nameof(Name)); }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
         }
 
-        private float _price;
+        private float _price = 0;
         public float Price
         {
             get { return _price; }
-            set { _price = value; NotifyPropertyChanged(nameof(Price)); }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    NotifyPropertyChanged(nameof(Price));
+                }
+            }
         }
-        
+
         private long _volume = 0;
         public long Volume
         {
             get { return _volume; }
-            set { _volume = value; NotifyPropertyChanged(nameof(Volume)); }
+            set
+            {
+                if (_volume != value)
+                {
+                    _volume = value;
+                    NotifyPropertyChanged(nameof(Volume));
+                }
+            }
         }
 
-        private float _previousClosedPrice;
+        private float _previousClosedPrice = 0;
         public float PreviousClosedPrice
         {
             get { return _previousClosedPrice; }
-            set { _previousClosedPrice = value; NotifyPropertyChanged(nameof(PreviousClosedPrice)); }
+            set
+            {
+                if (_previousClosedPrice != value)
+                {
+                    _previousClosedPrice = value;
+                    NotifyPropertyChanged(nameof(PreviousClosedPrice));
+                }
+            }
         }
 
-        private long _previousVolume;
+        private long _previousVolume = 0;
         public long PreviousVolume
         {
             get { return _previousVolume; }
-            set { _previousVolume = value; NotifyPropertyChanged(nameof(PreviousVolume)); }
+            set
+            {
+                if (_previousVolume != value)
+                {
+                    _previousVolume = value;
+                    NotifyPropertyChanged(nameof(PreviousVolume));
+                }
+            }
         }
 
         public DashboardItem()
