542fcaa8110ab16c947eb63b613998d30d7b32ea
truhojun <truhojun@gmail.com>
2016-04-03 21:07:17 +0900

    DataCompare 체결방식 변경
    호가 비교방식 -> 체결가 비교방식

--- Src/MTree.DataCompare/MainWindow.xaml.cs
+++ Src/MTree.DataCompare/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace MTree.DataCompare
 
             string code = "005930";
 #if true
-            IDataCollector source = new DbCollector(DbAgent.Instance);
+            IDataCollector source = new DbCollector(DbAgent.RemoteInstance);
             //IDataCollector destination = new DbCollector(DbAgent.RemoteInstance);
             IDataCollector destination = new DaishinCollector();
 
