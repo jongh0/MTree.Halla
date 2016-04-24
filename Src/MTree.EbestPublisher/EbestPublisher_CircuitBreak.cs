3113daa70c35a1b28aed709bc4e09be0db903382
Jongho Yang <jongho.yang@gmail.com>
2016-04-24 17:46:18 +0900

    CircuitBreak debugging code 추가

--- Src/MTree.EbestPublisher/EbestPublisher_CircuitBreak.cs
+++ Src/MTree.EbestPublisher/EbestPublisher_CircuitBreak.cs
@@ -84,26 +84,30 @@ namespace MTree.EbestPublisher
                 var circuitBreak = new CircuitBreak();
                 circuitBreak.Id = ObjectId.GenerateNewId();
                 circuitBreak.Time = circuitBreak.ReceivedTime = DateTime.Now;
-                var time = subscribingObj.GetFieldData("OutBlock", "time");
-                logger.Info($"Circuit Break Time Format:{time}"); // For debug
+
                 circuitBreak.Code = subscribingObj.GetFieldData("OutBlock", "shcode");
 
                 var circuitBreakType = Convert.ToInt32(subscribingObj.GetFieldData("OutBlock", "vi_gubun"));
+                var time = subscribingObj.GetFieldData("OutBlock", "time");
+                var svi_recprice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "svi_recprice"));
+                var dvi_recprice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice"));
+                logger.Info($"CircuitBreak debugging, type: {circuitBreakType}, time: {time}, svi_recprice: {svi_recprice}, dvi_recprice: {dvi_recprice}"); // For debugging
+
                 switch (circuitBreakType)
                 {
                     case 1:
                         circuitBreak.CircuitBreakType = CircuitBreakTypes.StaticInvoke;
-                        circuitBreak.InvokeBasisPrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "svi_recprice"));
+                        circuitBreak.InvokeBasisPrice = svi_recprice;
                         break;
 
                     case 2:
                         circuitBreak.CircuitBreakType = CircuitBreakTypes.DynamicInvoke;
-                        circuitBreak.InvokeBasisPrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice"));
+                        circuitBreak.InvokeBasisPrice = dvi_recprice;
                         break;
 
                     case 3:
                         circuitBreak.CircuitBreakType = CircuitBreakTypes.StaticAndDynamicInvoke;
-                        circuitBreak.InvokeBasisPrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice")); // TODO: dvi_recprice 맞나?
+                        circuitBreak.InvokeBasisPrice = dvi_recprice; // TODO: dvi_recprice 맞나?
                         break;
 
                     default: // 0
