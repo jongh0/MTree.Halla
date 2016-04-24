50566a5069ffcfad6b8f09ae3144807980a91f42
Jongho Yang <jongho.yang@gmail.com>
2016-04-24 17:52:25 +0900

    CircuitBreak debugging code 추가

--- Src/MTree.EbestPublisher/EbestPublisher_CircuitBreak.cs
+++ Src/MTree.EbestPublisher/EbestPublisher_CircuitBreak.cs
@@ -88,26 +88,21 @@ namespace MTree.EbestPublisher
                 circuitBreak.Code = subscribingObj.GetFieldData("OutBlock", "shcode");
 
                 var circuitBreakType = Convert.ToInt32(subscribingObj.GetFieldData("OutBlock", "vi_gubun"));
-                var time = subscribingObj.GetFieldData("OutBlock", "time");
-                var svi_recprice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "svi_recprice"));
-                var dvi_recprice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice"));
-                logger.Info($"CircuitBreak debugging, type: {circuitBreakType}, time: {time}, svi_recprice: {svi_recprice}, dvi_recprice: {dvi_recprice}"); // For debugging
-
                 switch (circuitBreakType)
                 {
                     case 1:
                         circuitBreak.CircuitBreakType = CircuitBreakTypes.StaticInvoke;
-                        circuitBreak.InvokeBasisPrice = svi_recprice;
+                        circuitBreak.InvokeBasisPrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "svi_recprice"));
                         break;
 
                     case 2:
                         circuitBreak.CircuitBreakType = CircuitBreakTypes.DynamicInvoke;
-                        circuitBreak.InvokeBasisPrice = dvi_recprice;
+                        circuitBreak.InvokeBasisPrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice"));
                         break;
 
                     case 3:
                         circuitBreak.CircuitBreakType = CircuitBreakTypes.StaticAndDynamicInvoke;
-                        circuitBreak.InvokeBasisPrice = dvi_recprice; // TODO: dvi_recprice 맞나?
+                        circuitBreak.InvokeBasisPrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice")); // TODO: dvi_recprice 맞나?
                         break;
 
                     default: // 0
@@ -117,6 +112,13 @@ namespace MTree.EbestPublisher
                 }
 
                 circuitBreak.InvokePrice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "vi_trgprice"));
+
+#if true // Debugging Code
+                var time = subscribingObj.GetFieldData("OutBlock", "time");
+                var svi_recprice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "svi_recprice"));
+                var dvi_recprice = Convert.ToSingle(subscribingObj.GetFieldData("OutBlock", "dvi_recprice"));
+                logger.Info($"CircuitBreak debugging, type: {circuitBreakType}, time: {time}, svi_recprice: {svi_recprice}, dvi_recprice: {dvi_recprice}, InvokePrice: {circuitBreak.InvokePrice}");  
+#endif
 
                 CircuitBreakQueue.Enqueue(circuitBreak);
             }
