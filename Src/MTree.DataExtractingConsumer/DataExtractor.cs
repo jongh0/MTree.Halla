5411fe7895435bdf5b25514d7db28b5308ef7a79
truhojun <truhojun@gmail.com>
2016-08-15 19:22:36 +0900

    Data Extracting Consumer 추가

---
+++ Src/MTree.DataExtractingConsumer/DataExtractor.cs
@@ -0,0 +1,70 @@
+﻿using MTree.Consumer;
+using MTree.DataStructure;
+using MTree.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MTree.DataExtractingConsumer
+{
+    public class DataExtractor
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        #region Queue Task
+        public CancellationToken QueueTaskCancelToken { get; set; }
+        #endregion
+
+        private ConsumerBase Consumer { get; set; }
+
+        public DataExtractor(ConsumerBase consumer)
+        {
+            try
+            {
+                Consumer = consumer;
+
+                Consumer.ConsumeStockMasterEvent += ConsumeStockMaster;                
+                TaskUtility.Run("Dashboard.StockConclusionQueue", QueueTaskCancelToken, ProcessStockConclusionQueue);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+        }
+        public void ConsumeStockMaster(List<StockMaster> stockMasters)
+        {
+            try
+            {
+                foreach (var stockMaster in stockMasters)
+                {
+         
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+        }
+        private void ProcessStockConclusionQueue()
+        {
+            try
+            {
+                StockConclusion conclusion;
+                if (Consumer.StockConclusionQueue.TryDequeue(out conclusion) == true)
+                {
+                }
+                else
+                {
+                    Thread.Sleep(10);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+            }
+        }
+
+    }
+}
---
+++ Src/MTree.DataExtractingConsumer/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MTree.Consumer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MTree.DataExtractingConsumer
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private string _Code;
+        public string Code
+        {
+            get { return _Code; }
+            set
+            {
+                _Code = value.Trim();
+                NotifyPropertyChanged(nameof(Code));
+            }
+        }
+
+        private DateTime _StartingDate = DateTime.Now;
+        public DateTime StartingDate
+        {
+            get { return _StartingDate; }
+            set
+            {
+                _StartingDate = value;
+
+                if (EndingDate < _StartingDate)
+                    EndingDate = _StartingDate;
+
+                NotifyPropertyChanged(nameof(StartingDate));
+            }
+        }
+
+        private DateTime _EndingDate = DateTime.Now;
+        public DateTime EndingDate
+        {
+            get { return _EndingDate; }
+            set
+            {
+                _EndingDate = value;
+
+                if (_EndingDate < StartingDate)
+                    StartingDate = _EndingDate;
+
+                NotifyPropertyChanged(nameof(EndingDate));
+            }
+        }
+
+        #region Command
+        private RelayCommand _StartExtractCommand;
+        public ICommand StartExtractCommand
+        {
+            get
+            {
+                if (_StartExtractCommand == null)
+                    _StartExtractCommand = new RelayCommand(() => Task.Run(() =>
+                    {
+                        string[] codes = { Code };
+                        for (DateTime targetDate = StartingDate; targetDate <= EndingDate; targetDate = targetDate.AddDays(1))
+                        {
+                            extractor.StartSimulation(codes, targetDate);
+                        }
+                    }));
+
+                return _StartExtractCommand;
+            }
+        }
+        #endregion
+
+        private ISimulation extractor { get; set; }
+        public MainViewModel()
+        {
+            extractor = new HistoryConsumer();
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
